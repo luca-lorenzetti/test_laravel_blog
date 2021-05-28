0f7ca324da0af99233a06a9d76cbd0a766716bf9
Beatrice Castagno <beatrice.castagno1@gmail.com>
2021-05-28 23:52:02 +0200

    creazione index e show dell'admin e rotta temporanea

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')->name('admin.')->namespace('Admin')->middleware('auth')->group( function () {
+    Route::get('/', function () {
+        return view('admin.posts.index');
+    });
+});
